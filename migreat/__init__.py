97b0eac4102740d57bf968faa6e5a4d5b8c279a9
João Sampaio <jpmelos@gmail.com>
2021-01-26 09:31:59 -0300

    Bump version to 0.1.3-dev

--- migreat/__init__.py
+++ migreat/__init__.py
@@ -7,7 +7,7 @@ from functools import cached_property
 
 import psycopg2
 
-__version__ = (0, 1, 1)
+__version__ = (0, 1, 3, 'dev')
 
 logger = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 version_regex = re.compile(
-    r"^__version__\s*=\s*\((?P<major>\d+), (?P<minor>\d+), "
-    r"(?P<revision>\d+)\)$",
-    re.MULTILINE,
+    r"^__version__\s*=\s*\((?P<major>\d+),\s*(?P<minor>\d+),"
+    r"\s*(?P<patch>\d+)"
+    r"(?:,\s*(?P<quote>[\"\'])(?P<prerelease>\w+)(?P=quote))?\)$",
 )
 
 
@@ -14,16 +14,19 @@ def find_version():
     version_file = Path("migreat/__init__.py")
     with version_file.open() as fp:
         for line in fp.readlines():
-            version_match = version_regex.search(line)
+            version_match = version_regex.fullmatch(line.strip())
             if version_match:
                 break
-    return ".".join(
-        [
-            version_match.group("major"),
-            version_match.group("minor"),
-            version_match.group("revision"),
-        ],
-    )
+
+    major = version_match.group("major")
+    minor = version_match.group("minor")
+    patch = version_match.group("patch")
+    prerelease = version_match.group("prerelease")
+
+    v = f'{major}.{minor}.{patch}'
+    if prerelease:
+        v += f'-{prerelease}'
+    return v
 
 
 def find_requires():
